0be877aa4cda7b0a7c06d9e65b546cb05ce38914
Dan <daniel@healthbridge.co.za>
2022-06-05 12:19:44 +0200

    Add header styles

--- src/app/includes/header/header.component.ts
+++ src/app/includes/header/header.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  links = [
+    'Portfolio',
+    'Bank',
+    'Medical aid',
+    'Gap cover',
+    'Life insurance',
+    'Investments',
+    'Car and home insurance',
+    'Vitality',
+    'Credit card',
+    'Reward partners',
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
